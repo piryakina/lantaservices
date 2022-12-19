2381cc75464b47991a95953085832450c63c52c4
piryakina <anya.piryakina@gmail.com>
2022-12-19 15:33:07 +0300

    commit back

--- lantaservice/webserver/news.go
+++ lantaservice/webserver/news.go
@@ -2,6 +2,7 @@ package webserver
 
 import (
 	"encoding/json"
+	"fmt"
 	"lantaservice/entities"
 	"lantaservice/usecase"
 	"net/http"
@@ -16,9 +17,10 @@ func AddNews(w http.ResponseWriter, r *http.Request) {
 		ErrorResponse(w, err)
 		return
 	}
-	date := time.Now()
-	l := "2001-06-5"
-	c.Date = date.Format(l)
+	date := time.Now().Format("02-01-2006")
+	//l := "2001-06-5"
+	c.Date = date
+	fmt.Println(c.Date)
 	id, err := usecase.AddNews(ctx, c)
 	if err != nil {
 		ErrorResponse(w, err)
